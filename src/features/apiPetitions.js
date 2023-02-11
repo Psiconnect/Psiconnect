bff7cfb17320c72e47173c589c47439a1682edeb
Braian Oscar Valdez <94503113+braianpx@users.noreply.github.com>
2023-02-11 19:43:03 -0300

    Update apiPetitions.js

--- src/features/apiPetitions.js
+++ src/features/apiPetitions.js
@@ -46,8 +46,11 @@ export async function profLogin(body) {
 
 export async function profUpdate(body){
   try {
-    const petition= await axios.put('/professional/descriptionProfesional', body);
-    localStorage.setItem("profTkn", petition?.data);
+    const petition= await axios.put('/professional/update/id', payload,{
+      headers: { authorization: `Bearer ${localStorage.getItem("profTkn")}` },
+    } );
+    type === "local" ? state(petition?.data) : state(setUser({...petition?.data, rol: 'professional'}));
+    console.log(petition?.data)
     return petition
 
   } catch (error) {
